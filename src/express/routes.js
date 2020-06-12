16ba4854a3d45b890cbcf39f0cfcd68fa114e917
Joshua Lee Wirtner <jlwirtner@gmail.com>
2020-06-12 11:36:55 -0700

    now reporting all info for connected sensors

--- src/express/routes.js
+++ src/express/routes.js
@@ -6,7 +6,16 @@ const api = express.Router();
 
 api.get('/', (req, res) => {
     console.log(req.app.settings.sensorHub.ctx.connectedSensors);
-    res.send({connectedSensors: [...req.app.settings.sensorHub.ctx.connectedSensors]});
+    var output = []
+    var connectedMacs = [...req.app.settings.sensorHub.ctx.connectedSensors]
+    for (var i = 0; i < connectedMacs.length; i++) {
+        for(var j = 0; j < req.app.settings.sensorHub.ctx.registeredSensors.length; j++) {
+            if(connectedMacs[i] === req.app.settings.sensorHub.ctx.registeredSensors[j].macAddress) {
+                output.push(req.app.settings.sensorHub.ctx.registeredSensors[j])
+            }
+        }
+    }
+    res.send({connectedSensors: output});
 });
 
 module.exports = api;
